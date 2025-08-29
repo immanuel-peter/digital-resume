27bd7a858e7fcd458074b13f69a68af7843728ad
immanuel-peter <immanuel.peter1@gmail.com>
2025-08-29 16:56:38 -0500

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,64 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: "Digital Resume - Immanuel Peter",
-  description: "Digital Resume - Immanuel Peter",
+  title: "Immanuel Peter",
+  description: "Immanuel Peter's personal website",
+  keywords: [
+    "Immanuel Peter",
+    "Immanuel",
+    "Peter",
+    "immanuelpeter",
+    "immanuel-peter",
+    "immanuelpeter.com",
+    "immanuel-peter.com",
+    "ImmanuelAI",
+    "ipeter",
+    "Immanuel Peter University of Chicago",
+    "Immanuel Peter uchicago",
+    "moby763canary21",
+
+    "Software Engineer Intern",
+    "Machine Learning Engineer Intern",
+    "Artificial Intelligence Engineer Intern",
+    "University of Chicago Computer Science",
+    "University of Chicago CS",
+    "University of Chicago Physics",
+    "University of Chicago Math",
+    "University of Chicago",
+  ],
+  creator: "Immanuel Peter",
+  authors: [{ name: "Immanuel Peter", url: "https://ipeter.tech" }],
+  openGraph: {
+    title: "Immanuel Peter",
+    description: "Immanuel Peter's personal website",
+    url: "https://ipeter.tech",
+    images: ["https://ipeter.tech/opengraph-image.png"],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Immanuel Peter",
+    description: "Immanuel Peter's personal website",
+    creator: "@moby763canary21",
+    images: ["https://ipeter.tech/twitter-image.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  category: "technology",
 };
 
 export default function RootLayout({
